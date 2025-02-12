3a25bee42915c971965751ecdece8e015200f8f1
Koolsdaad <t99962018@gmail.com>
2025-02-11 21:17:25 -0300

    Update and rename Khanware.js to Lunaris.js

--- Lunaris.js
+++ Lunaris.js
@@ -1,5 +1,5 @@
 const ver = "V0.0.1";
-const repoPath = "https://raw.githubusercontent.com/Koolsdaad/khanescolam/refs/heads/main/"
+const repoPath = "https://raw.githubusercontent.comKoolsdaad/Lunaris-test/refs/heads/main/"
 
 
 let device = {
